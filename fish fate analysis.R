4b93842187eff4f84fd4f61e0a234defbff54267
Garth Covernton <GCov@users.noreply.github.com>
2025-03-11 19:17:59 -0400

    Finalized spectroscopy correction and count summaries

--- fish fate analysis.R
+++ fish fate analysis.R
@@ -66,7 +66,7 @@ str(SaR)
 
 ##### Set up the data #####
 
-# First separate out just the particles
+# First separate out just the particles and look at them in a few ways
 
 fish_particles <-
   fish2 %>% 
@@ -90,6 +90,15 @@ spectroscopy_particles <-
 summary(spectroscopy_particles$count_ID)
 summary(spectroscopy_particles$spectroscopy_method)  # decent data for each method
 summary(spectroscopy_particles$spectroscopy_ID)
+
+ggplot(spectroscopy_particles, 
+       aes(x = count_ID, 
+           fill = spectroscopy_ID)) + 
+  geom_bar() + 
+  facet_grid(spectroscopy_method ~ organ, scales = "free_y") + 
+  labs(x = "Polymer Identified by Colour", y = "Number of particles") +
+  scale_fill_viridis_d(option = "turbo",
+                       name = "Spectroscopy ID")
 
 # Make a new column for category
 
@@ -163,7 +172,7 @@ rf2 <- randomForest(cat ~ organ + count_ID + fish_ID,
                    proximity = TRUE,
                    ntree = 251)
 
-rf2  # OOB error rate = 9.64%%
+rf2  # OOB error rate = 10.04%%
 
 train$predict <- predict(rf2)
 
@@ -219,7 +228,7 @@ plot(rf3)
 rf4 <- randomForest(cat ~ count_ID + organ,
                     data = train,
                     proximity = TRUE,
-                    ntree = 501)
+                    ntree = 1001)
 
 rf4  # OOB error rate = 9.24%
 
@@ -247,6 +256,9 @@ ggplot(no_spectroscopy_particles,
                     values = c("grey", "yellow", "blue", "pink")) +
   facet_wrap(~ organ, scales = "free_y") +
   theme_bw()
+
+# So basically the only 'correction' this has made is to remove all PS from the
+# fillet samples that didn't have spectroscopy done.
 
 fish3 <-
   fish2 %>% 
@@ -277,13 +289,17 @@ ggplot(fish3 %>% filter(!is.na(polymer)),
            fill = polymer)) +
   geom_bar() +
   facet_wrap(~ nominal_MPs, scales = "free_y") +
+  scale_fill_manual(name = "Assigned Polymer",
+                    values = c("grey", "yellow", "blue", "pink")) +
   theme_bw()
 
 ggplot(fish3 %>% filter(!is.na(polymer)),
        aes(x = organ,
-           fill = count_ID)) +
+           fill = polymer)) +
   geom_bar() +
   facet_wrap(~ fish_ID, scales = "free_y") +
+  scale_fill_manual(name = "Assigned Polymer",
+                    values = c("grey", "yellow", "blue", "pink")) +
   theme_bw()
 
 #### Summarize New Data ####
@@ -300,7 +316,7 @@ fish4 <-
                            1,
                            0))
 
-fish4_particle <-
+fish4_particles <-
   fish4 %>% 
   filter(!is.na(particle))
 
@@ -310,71 +326,63 @@ fish4_particle <-
 fish_new <-
   expand.grid(fish_ID = unique(fish4$fish_ID),
               organ = unique(fish4$organ),
-              polymer = unique(fish4$polymer)[-c(1,4)]) %>%
-  left_join(fish4 %>% 
-              select(fish_ID,
-                     organ,
-                     sample_ID, 
-                     corral, 
-                     nominal_MPs, 
-                     total_length, 
-                     weight, 
-                     GIT_weight, 
-                     fillet_weight, 
-                     liver_weight, 
-                     gill_weight) %>% 
-              group_by(fish_ID,
-                       organ,
-                       sample_ID, 
-                       corral, 
-                       nominal_MPs, 
-                       total_length, 
-                       weight, 
-                       GIT_weight, 
-                       fillet_weight, 
-                       liver_weight, 
-                       gill_weight) %>% 
-              summarize(dummy = length(fish_ID)) %>% 
-              ungroup(),
-            by = c("fish_ID", "organ")) %>% 
-  full_join(fish4_particle) %>% 
-  filter(polymer != "Contamination")
+              polymer = unique(fish4$polymer)[-c(1,4)])
 
-fish_new$particle[is.na(fish_new$particle)] <- 0
+# Add in sample data
 
-# Create a summary dataset
-
-fish_summary <-
-  fish_new %>% 
-  group_by(fish_ID,
+fish_data <-
+  fish4 %>% 
+  group_by(corral,
+           fish_ID,
            organ,
-           corral,
            nominal_MPs,
            total_length,
            weight,
            GIT_weight,
            fillet_weight,
            liver_weight,
-           gill_weight,
+           gill_weight) %>% 
+  summarize(dummy = length(sample_ID),
+            .groups = "drop")
+
+# Combine into new data
+
+fish_new <-
+  fish_new %>% 
+  left_join(fish_data, 
+            by = c("fish_ID", "organ"))
+
+# Summarise counts of fish particles
+
+fish_particle_summary <-
+  fish4_particles %>% 
+  group_by(fish_ID,
+           organ,
            polymer) %>% 
-  summarize(count = sum(particle)) %>% 
+  summarize(count = sum(particle),
+            .groups = "drop") %>% 
   ungroup()
 
-fish_summary$polymer <- as.character(fish_summary$polymer)
-fish_summary$polymer <- as.factor(fish_summary$polymer)
-
-str(fish_summary)
+fish_full_summary <-
+  fish_new %>% 
+  left_join(fish_particle_summary,
+            by = c("fish_ID",
+                   "organ",
+                   "polymer")) %>% 
+  mutate(count = replace_na(count, 0))
 
 # Plot
 
-ggplot(fish_summary,
-       aes(x = nominal_MPs,
+ggplot(fish_full_summary,
+       aes(x = as.factor(as.character(nominal_MPs)),
            y = count,
            fill = polymer)) +
+  geom_boxplot() +
   geom_point(shape = 21) +
-  scale_x_continuous(trans = "log1p",
+  scale_y_continuous(trans = "log1p",
                      breaks = c(0, 1, 100, 1000, 10000)) +
-  facet_grid(polymer ~ organ) +
+  facet_grid(organ ~ polymer,
+             scales = "free_y") +
   labs(y = "MPs per ind",
        x = "Nominal Exposure Concentration MPs") +
   scale_fill_manual(values = c("yellow", "blue", "pink"),
@@ -384,16 +392,45 @@ ggplot(fish_summary,
 # Summary stats
 
 fish_totals <-
-  fish_summary %>% 
+  fish_full_summary %>% 
   group_by(fish_ID, organ, nominal_MPs) %>% 
-  summarize(total_count = sum(count)) %>% 
-  ungroup()
+  summarize(total_count = sum(count),
+            .groups = "drop")
 
 fish_means <-
   fish_totals %>% 
   group_by(organ, nominal_MPs) %>% 
   summarize(mean = mean(total_count),
-            sd = sd(total_count)) %>% 
-  ungroup()
+            sd = sd(total_count),
+            .groups = "drop")
 
 fish_means
+
+# Plot
+
+ggplot(fish_totals,
+       aes(x = nominal_MPs,
+           y = total_count,
+           group = nominal_MPs, 
+           fill = as.factor(nominal_MPs))) +
+  geom_boxplot() +
+  geom_jitter(shape = 21,
+              height = 0) +
+  scale_y_continuous(trans = "log1p",
+                     breaks = c(0, 1, 10, 100, 1000, 10000)) +
+  scale_x_continuous(trans = "log1p",
+                     breaks = c(0, 1, 10, 100, 1000, 10000)) +
+  facet_wrap(~ organ,
+             scales = "free_y") +
+  labs(y = "MPs per ind",
+       x = "Nominal Exposure Concentration MPs") +
+  scale_fill_viridis_d(option = "inferno",
+                       direction = -1,
+                       name = "") +
+  theme_bw()
+
+#### Modeling ####
+
+# See how organ concentration relate to nominal exposure concentrations
+
+
